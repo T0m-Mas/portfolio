7a48e5370abfc5746e2b2f98d8c60de5879bf609
Tomás Masotta <73763881+T0m-Mas@users.noreply.github.com>
2026-06-11 10:27:35 -0300

    feat: maqueta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "../styles/globals.css";
+import SideMenu from "@/components/SideMenu";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,8 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html lang="sp">
+      <body>
+        <SideMenu />
+        <div className="content-container">{children}</div>
+      </body>
     </html>
   );
 }
